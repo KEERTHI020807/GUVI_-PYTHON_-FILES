ce010c5dffa165b655eb41da591e2b5555af2950
KEERTHI020807 <kk6087@srmist.edu.in>
2025-10-17 12:17:01 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+'''lst=[1,2,3,4,5]
+target=4
+if(target=4)
+print("target is present)'''
+
+'''y=int(input("enter the number"))
+fact=1
+for i in range(1,y+1):
+    fact=fact*i
+    print("the factorial of",y,"is =",fact)'''
+
+'''def rev(str1):
+    str2=''
+    for i in str1:
+        str2=i+str2
+    if(str1==str2):
+            print("it is palindrone")
+    else:
+            print("it is not palindrone")
+x=input("enter a string:")
+rev(x)'''
+                 
+'''a="banana"
+count=0
+for i in a:
+    if i =='a':
+        count+=1
+print("the no of a in word=",count)'''
+
+'''sum=0
+for i in  range(0,11,2):
+    x=i**2
+    sum=sum+x
+print(sum)'''
+
+
